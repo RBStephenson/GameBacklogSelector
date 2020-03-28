7062e040c4ad6699d1755cd95e165c9c5624bfd6
RBStephenson <brentstephenson@homtail.com>
2020-03-28 13:17:24 -0400

    Initial commit and project creation. Mainly used for testing.

---
+++ GameBacklogSelector/GameBacklogSelector.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameBacklogSelector.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadLine();
+        }
+    }
+}
