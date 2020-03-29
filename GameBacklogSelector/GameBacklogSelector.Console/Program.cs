e70ec1006236fff1cd93d2e7241a70e6fad3cb55
RBStephenson <brentstephenson@homtail.com>
2020-03-29 18:27:06 -0400

    * Minor refactoring - need to make sure that I don't do anything with the code until my son and I are working on it together. * Added additional TODOs for new features to add.

--- GameBacklogSelector/GameBacklogSelector.Console/Program.cs
+++ GameBacklogSelector/GameBacklogSelector.Console/Program.cs
@@ -10,12 +10,19 @@ namespace GameBacklogSelector.Console
     {
         static void Main(string[] args)
         {
-            // TODO: Check to see if there are subdirectories
-            // TODO: Loop through subdirectories (recursive)
-            // TODO: Look for executables (filename.exe)
-            // TODO: Create a list of executables
-            // TODO: Pick a random executable from the list of executables
-            
+            // TODO: Add a way to mark a game as complete
+            // TODO: Add a way to mark a game as in progress (or now playing)
+            // TODO: Store results from complete and now playing
+            // TODO: Add a way to skip games that are marked as complete or now playing
+            // TODO: Add a way to exclude files (blacklist) from file
+            // TODO: Add a way to exclude directories from file
+            // TODO: Add message "You are currently playing X games" and list the game titles
+            // TODO: Add a message "You have completed X games" and list the game titles
+            // TODO: Find a way to get the game's title and box art to display (for UI, not console app)
+
+            if (args == null) throw new ArgumentNullException(nameof(args));
+            if (args.Length == 0) throw new ArgumentException("Value cannot be an empty collection.", nameof(args));
+
             // Parse out the command line arguments and create a list of directories to search
             var directories = args.ToList();
             var gameInfoList = new List<KeyValuePair<string, string>>();
@@ -41,9 +48,9 @@ namespace GameBacklogSelector.Console
 
                         if (currentDir.Contains("downloading", StringComparison.OrdinalIgnoreCase) ||
                             currentDir.Contains("shadercache", StringComparison.OrdinalIgnoreCase) ||
-                            currentDir.Contains("temp", StringComparison.OrdinalIgnoreCase) || 
+                            currentDir.Contains("temp", StringComparison.OrdinalIgnoreCase) ||
                             currentDir.Contains(@"\Mono\", StringComparison.OrdinalIgnoreCase) ||
-                            currentDir.Contains(@"StreamingAssets", StringComparison.OrdinalIgnoreCase)||
+                            currentDir.Contains(@"StreamingAssets", StringComparison.OrdinalIgnoreCase) ||
                             currentDir.Contains(@"_windows", StringComparison.OrdinalIgnoreCase) ||
                             currentDir.Contains(@"\jre\", StringComparison.OrdinalIgnoreCase))
                         {
@@ -55,41 +62,52 @@ namespace GameBacklogSelector.Console
                         string[] files = null;
                         files = Directory.GetFiles(currentDir, "*.exe");
 
-                        foreach (string file in files)
+                        foreach (var file in files)
                         {
                             // TODO: Put these files in a "blacklist" config file
-                            if (file.Contains("setup", StringComparison.OrdinalIgnoreCase) || file.Contains("install", StringComparison.OrdinalIgnoreCase) || 
-                                file.Contains("crash", StringComparison.OrdinalIgnoreCase) || file.Contains("launcher", StringComparison.OrdinalIgnoreCase) ||
-                                file.Contains("java", StringComparison.OrdinalIgnoreCase) || file.Contains("bootstrap", StringComparison.OrdinalIgnoreCase) ||
-                                file.Contains("vc_redist", StringComparison.OrdinalIgnoreCase) || file.Contains("wow_helper", StringComparison.OrdinalIgnoreCase) ||
-                                file.Contains("smcs", StringComparison.OrdinalIgnoreCase) || file.Contains("vcredist", StringComparison.OrdinalIgnoreCase) ||
-                                file.Contains("dotNetFx40", StringComparison.OrdinalIgnoreCase) || file.Contains("CefSharp", StringComparison.OrdinalIgnoreCase) ||
-                                file.Contains("mono-", StringComparison.OrdinalIgnoreCase) || file.Contains("gacutil", StringComparison.OrdinalIgnoreCase) ||
-                                file.Contains("csharp", StringComparison.OrdinalIgnoreCase) || file.Contains("sql", StringComparison.OrdinalIgnoreCase) ||
-                                file.Contains("svcutil", StringComparison.OrdinalIgnoreCase) || file.Contains("xsd", StringComparison.OrdinalIgnoreCase) ||
-                                file.Contains("wsdl", StringComparison.OrdinalIgnoreCase) || file.Contains("xbuild", StringComparison.OrdinalIgnoreCase) ||
-                                file.Equals("lc.exe") || file.Equals("Application-steam-x32.exe") || file.Equals("UnrealCEFSubProcess.exe") || file.Equals("breakpad_server.exe"))
+                            if (file.Contains("setup", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("install", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("crash", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("launcher", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("java", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("bootstrap", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("vc_redist", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("wow_helper", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("smcs", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("vcredist", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("dotNetFx40", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("CefSharp", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("mono-", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("gacutil", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("csharp", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("sql", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("svcutil", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("xsd", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("wsdl", StringComparison.OrdinalIgnoreCase) ||
+                                file.Contains("xbuild", StringComparison.OrdinalIgnoreCase) ||
+                                file.Equals("lc.exe") || file.Equals("Application-steam-x32.exe") ||
+                                file.Equals("UnrealCEFSubProcess.exe") || file.Equals("breakpad_server.exe"))
                                 continue;
-                            
+
                             if ((File.GetAttributes(file) & FileAttributes.Hidden) == FileAttributes.Hidden ||
                                 (File.GetAttributes(file) & FileAttributes.System) == FileAttributes.System)
                             {
                                 continue;
                             }
 
-                            FileInfo fi = new FileInfo(file);
-                            System.Console.WriteLine($"{fi.Name} ({fi.DirectoryName})");
+                            var fi = new FileInfo(file);
+                            // System.Console.WriteLine($"{fi.Name} ({fi.DirectoryName})");
                             gameInfoList.Add(new KeyValuePair<string, string>(fi.Name, fi.DirectoryName));
                         }
-                    
-                        foreach(string str in subdirectories)
+
+                        foreach (var str in subdirectories)
                             dirs.Push(str);
                     }
                 }
             }
 
             var random = new Random();
-            int index = random.Next(gameInfoList.Count);
+            var index = random.Next(gameInfoList.Count);
 
             System.Console.WriteLine();
             System.Console.WriteLine("================================================");
